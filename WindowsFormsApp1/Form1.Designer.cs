7ed9570e00207a4b590d0de7a02ed2bc2b07b477
agustin-giuliani <agustingiuliani45giuliani@gmail.com>
2018-09-13 20:49:17 -0300

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pitb1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pitb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pitb1
+            // 
+            this.pitb1.Location = new System.Drawing.Point(0, 3);
+            this.pitb1.Name = "pitb1";
+            this.pitb1.Size = new System.Drawing.Size(712, 454);
+            this.pitb1.TabIndex = 0;
+            this.pitb1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(260, 204);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 38);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(492, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 41);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(495, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(207, 23);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Aqua;
+            this.label4.Location = new System.Drawing.Point(101, 394);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 47);
+            this.label4.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(512, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 64);
+            this.label5.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label6.Location = new System.Drawing.Point(25, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 30);
+            this.label6.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 461);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pitb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pitb1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pitb1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        //permite el uso del picture box no tocar
+        Graphics lienzo;
+        int x = 0, y = 0;
+        Image imagen = Image.FromFile("rock.png");
+        
+        public Form1()
+        {
+            //permite que el se mueba el objeto
+            InitializeComponent();
+            lienzo = pitb1.CreateGraphics();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //movimiento de los cuadrados
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+            if (label1.Left >= pitb1.Width)
+            {
+                label1.Left = 0;
+            }
+            label1.Left = label1.Left + 8;
+
+            label2.Left = label2.Left + 8;
+            if (label2.Left >= pitb1.Width)
+            {
+                label2.Left = 0;
+            }
+
+            label4.Left = label4.Left + 8;
+            if (label4.Left >= pitb1.Width)
+            {
+                label4.Left = 0;
+            }
+
+            label5.Left = label5.Left + 8;
+            if (label5.Left >= pitb1.Width)
+            {
+                label5.Left = 0;
+            }
+
+            label6.Left = label6.Left + 8;
+            if (label6.Left >= pitb1.Width)
+            {
+                label6.Left = 0;
+            }
+
+
+        }
+
+
+        //controles del la imagen por botones
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyData)
+            {
+                case Keys.Down:
+                    if(y + imagen.Height+10<pitb1.Height)
+                         y += 10;
+                    break;
+
+                case Keys.Left:
+                    if(x-10>0)
+                        x -= 10;  
+                    break;
+
+                case Keys.Right:
+                    if(x+10+imagen.Width<pitb1.Width)
+                         x += 10;
+                    break;
+
+                case Keys.Up:
+                    if(y-10>0)
+                    y -= 10;
+
+                    break;
+                default:
+                    break;
+            }
+            lienzo.Clear(pitb1.BackColor);
+            lienzo.DrawImage(imagen, x, y);
+
+        }
+
+
+    }
+
+}
